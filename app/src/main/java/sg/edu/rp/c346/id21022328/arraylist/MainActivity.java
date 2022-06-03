6b081999fac1fa5831ef5a255aed145965dec1ad
21022328 <21022328@myrp.edu.sg>
2022-06-03 13:11:58 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id21022328/arraylist/MainActivity.java
@@ -0,0 +1,39 @@
+package sg.edu.rp.c346.id21022328.arraylist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+    ArrayList<String> fruits;
+    String theFruit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv = findViewById(R.id.tv);
+
+
+        fruits = new ArrayList<String>();
+        fruits.add("apple");
+        fruits.add("banana");
+        fruits.add("cherry");
+        fruits.add("durian");
+
+
+        tv.setText(String.format("%s\n",fruits.get(1)));
+        theFruit = fruits.get(1);
+        fruits.remove(0);
+        fruits.set(fruits.size() -1, "dragon fruit");
+
+
+
+    }
+}
